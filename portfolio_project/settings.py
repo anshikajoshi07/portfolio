241fc428d8cf3120e3a1828d4cb352d48126969d
anshika joshi <anshikajoshi1823@gmail.com>
2026-05-03 10:02:42 +0530

    Fix Django static config and Render build command

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -123,9 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'portfolio_app' / 'static',
-]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
